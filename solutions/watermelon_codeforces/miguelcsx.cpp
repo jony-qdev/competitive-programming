582e3decac6d26f7682c5a8db18c3e8578e5c58d
Miguel Cardenas <miguelecsx@gmail.com>
2024-03-11 19:33:35 -0400

    feat: solution to codeforces 4a - A. Watermelon

---
+++ solutions/watermelon_codeforces/miguelcsx.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main() {
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int w;
+
+	cin >> w;
+
+	if ((w - 2) % 2 == 0) {
+		cout << "YES";
+	}
+	else {
+		cout << "NO";
+	}
+
+	return 0;
+}
